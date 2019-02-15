a45877b2fcd81665c878203bf946bb6114cf3043
Dmitry <dkruban@gmail.com>
2019-02-15 23:58:30 +0300

    the example of project with qudo

---
+++ examples/proposal-deep-libs/lib/roda_app_api/components/client.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'qudo/component'
+
+module RodaAppApi
+  module Components
+    # Simple HTTP client for selected resource
+    class Client < Qudo::Component
+      config do
+        property 'resource', required: true
+      end
+
+      builder do |config|
+        require 'rest-client'
+
+        RestClient::Resource.new config.resource
+      end
+    end
+  end
+end
---
+++ examples/proposal-deep-libs/lib/roda_app_api/router.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'roda'
+require_relative './application'
+
+module RodaAppApi
+  # @see https://roda.jeremyevans.net/
+  class Router < Roda
+    plugin :json
+
+    route do |r|
+      r.root { 'Hello world!' }
+
+      r.get 'films' do
+        JSON.parse Application.container[:client].resolve['/films'].get
+      end
+    end
+  end
+end
